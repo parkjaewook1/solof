f4883aeacd27f8457bdaacba296834c41c24658a
parkjaewook1 <wodnr5050@gmail.com>
2024-07-15 16:39:27 +0900

    전송 중 버튼 비활성화

--- src/page/board/BoardWrite.jsx
+++ src/page/board/BoardWrite.jsx
@@ -5,23 +5,55 @@ import {
   FormLabel,
   Input,
   Textarea,
+  useToast,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export function BoardWrite() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [writer, setWriter] = useState("");
+  const [loading, setLoading] = useState(false);
+  const toast = useToast();
+  const navigate = useNavigate();
 
   function handleSaveClick() {
-    axios.post("/api/board/add", {
-      title: title,
-      content: content,
-      writer: writer,
-    });
+    setLoading(true);
+    axios
+      .post("/api/board/add", {
+        title: title,
+        content: content,
+        writer: writer,
+      })
+      .then(() => {
+        toast({
+          status: "success",
+          description: "등록 성공!",
+          position: "top",
+        });
+        navigate("/");
+      })
+      .catch((e) => {
+        if (e.response.status === 400) {
+          toast({
+            status: "error",
+            description: "등록 실패",
+            position: "top",
+          });
+        }
+      })
+      .finally(() => setLoading(false));
   }
 
+  let disableSaveButton = false;
+  if (title.trim().length === 0) {
+    disableSaveButton = true;
+  }
+  if (content.trim().length === 0) {
+    disableSaveButton = true;
+  }
   return (
     <Box>
       <Box>글 작성 화면</Box>
@@ -45,7 +77,12 @@ export function BoardWrite() {
           </FormControl>
         </Box>
         <Box>
-          <Button colorScheme={"blue"} onClick={handleSaveClick}>
+          <Button
+            isLoading={loading}
+            colorScheme={"blue"}
+            onClick={handleSaveClick}
+            isDisabled={disableSaveButton}
+          >
             저장
           </Button>
         </Box>
