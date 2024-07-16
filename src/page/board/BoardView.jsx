fc169663693484b66812fc1e9a7e40f21d647008
parkjaewook1 <wodnr5050@gmail.com>
2024-07-16 15:18:07 +0900

    front: 상세창, 수정 기능 추가 모달로 재확인 기능 추가, App에 Edit 경로 추가

--- src/page/board/BoardView.jsx
+++ src/page/board/BoardView.jsx
@@ -3,11 +3,18 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import {
   Box,
+  Button,
   FormControl,
   FormLabel,
   Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalOverlay,
   Spinner,
   Textarea,
+  useDisclosure,
   useToast,
 } from "@chakra-ui/react";
 
@@ -16,6 +23,7 @@ export function BoardView() {
   const toast = useToast();
   const navigate = useNavigate();
   const { id } = useParams();
+  const { isOpen, onOpen, onClose } = useDisclosure();
   useEffect(() => {
     axios
       .get(`/api/board/${id}`)
@@ -35,6 +43,30 @@ export function BoardView() {
   if (board === null) {
     return <Spinner />;
   }
+
+  function handleClickRemove() {
+    axios
+      .delete(`/api/board/${id}`)
+      .then(() => {
+        toast({
+          status: "success",
+          description: `${id}번 게시물 삭제가 완료되었습니다.`,
+          position: "top",
+        });
+        navigate("/");
+      })
+      .catch(() => {
+        toast({
+          status: "error",
+          description: `${id}번 게시물 삭제 중 오류가 발생하였습니다.`,
+          position: "top",
+        });
+      })
+      .finally(() => {
+        onClose();
+      });
+  }
+
   return (
     <Box>
       <Box>{board.id}번 게시물</Box>
@@ -60,6 +92,31 @@ export function BoardView() {
         <FormControl>작성일시</FormControl>
         <Input type={"datetime-local"} value={board.inserted} readOnly />
       </Box>
+      <Box>
+        <Button
+          colorScheme={"blue"}
+          onClick={() => navigate(`/edit/${board.id}`)}
+        >
+          수정
+        </Button>
+        <Button colorScheme={"red"} onClick={onOpen}>
+          삭제
+        </Button>
+      </Box>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalBody>삭제하시겠습니까?</ModalBody>
+          <ModalFooter>
+            <Button colorScheme={"blue"} onClick={onClose}>
+              취소
+            </Button>
+            <Button colorScheme={"red"} onClick={handleClickRemove}>
+              확인
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
