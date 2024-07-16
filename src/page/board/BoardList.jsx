5bc20c7b15d41f92e4f03d8d359a5a05ad24bacf
parkjaewook1 <wodnr5050@gmail.com>
2024-07-16 11:35:48 +0900

    front: boardView,List 수정 게시물 없을 때 404 응답 하도록 설정

--- src/page/board/BoardList.jsx
+++ src/page/board/BoardList.jsx
@@ -1,10 +1,17 @@
 import { Box, Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserPen } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export function BoardList() {
   const [boardList, setBoardList] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios.get("/api/board/list").then((res) => setBoardList(res.data));
+  }, []);
 
   return (
     <Box>
@@ -22,7 +29,14 @@ export function BoardList() {
           </Thead>
           <Tbody>
             {boardList.map((board) => (
-              <Tr key={board.id}>
+              <Tr
+                onClick={() => navigate(`/board/${board.id}`)}
+                key={board.id}
+                cursor={"pointer"}
+                _hover={{
+                  bgColor: "blue.200",
+                }}
+              >
                 <Td>{board.id}</Td>
                 <Td>{board.title}</Td>
                 <Td>{board.writer}</Td>
